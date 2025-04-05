84eeb8479fc80e83d6c4f7d908679fe81eeb16c3
rob <rjgallac@hotmail.com>
2025-04-05 10:26:10 +0100

    try running in background again

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import logging
 app = Flask(__name__)
-logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logging.basicConfig(filename='pythonflasksensor.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route("/")
 def hello_world():
