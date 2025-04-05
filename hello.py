38880a7444e0e4400434a34a50da8e8050ef8152
rob <rjgallac@hotmail.com>
2025-04-05 10:19:31 +0100

    logging

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
-
+import logging
 app = Flask(__name__)
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route("/")
 def hello_world():
+
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
+
     return "<p>Hello, World!</p>"
--- runServer.sh
+++
@@ -1,1 +0,0 @@
-flask --app hello run 
